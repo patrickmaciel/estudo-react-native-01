a3cfa66622fa223d8534a54ecfcd9970d95bb0d0
Patrick Maciel <patrickmaciel.info@gmail.com>
2018-05-21 14:10:21 -0300

    API GET Request e apresentação dos albums em formato texto

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default class AlbumList extends React.Component {
+    state = {
+        albums: []
+    };
+
+    // https://rallycoding.herokuapp.com/api/music_albums
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({albums: response.data}));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <Text key={album.title}>{album.title}</Text>);
+    }
+
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
